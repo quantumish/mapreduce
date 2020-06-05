399954b7b3db603e094c020b80839e243ba7ac6b
David Freifeld <freifeld.david@gmail.com>
2020-06-05 12:51:13 -0700

    Added checking of IP for deciding where to start server

--- mapreduce.c
+++ mapreduce.c
@@ -82,6 +82,24 @@ void begin(char* path, struct int_pair * (*map)(struct str_pair), struct int_pai
   // no joining besides that thread is necessary. Source for POSIX threading library:
   // https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html
 
+  FILE* fp;
+  path[1035];
+  fp = popen("/usr/bin/dig +short myip.opendns.com @resolver1.opendns.com", "r");
+  if (fp == NULL) {
+    printf("Failed to run dig\n" );
+    exit(1);
+  }
+  char comp_ip[15];
+  while (fgets(path, sizeof(path), fp) != NULL) {
+    strcat(comp_ip, path);
+  }
+  for (int i = 0; i < 15; i++) {
+    if (comp_ip[i] == '\n') {
+      comp_ip[i] = '\0';
+    }
+  }
+  printf("MAINLIB | Found IP to be %s\n", comp_ip);
+  pclose(fp);
 
   int part1, part2, part3, part4;
   part1=part2=part3=part4=0;
@@ -89,10 +107,11 @@ void begin(char* path, struct int_pair * (*map)(struct str_pair), struct int_pai
   int ip_int = (part1 * pow(256, 3)) + (part2 * pow(256, 2)) + (part3 * 256) + (part4);
 
   pthread_t server;
-  int ret1;
-  ret1 = pthread_create(&server, NULL, start_server, (void *) m);
-  printf("MAINLIB | Created server thread.\n");
-
+  /* if (strcmp(comp_ip, ip) == 0) { */
+    int ret1;
+    ret1 = pthread_create(&server, NULL, start_server, (void *) m);
+    printf("MAINLIB | Created server thread.\n");
+  /* } */
 
   for (int i = 0; i < m; i++) {
     pthread_t worker;
@@ -105,9 +124,13 @@ void begin(char* path, struct int_pair * (*map)(struct str_pair), struct int_pai
     pass_args->ip = ip_int;
     pthread_create(&worker, NULL, start_worker, (void *) pass_args);
     printf("MAINLIB | Created worker thread #%i.\n", i);
-
+    if (strcmp(comp_ip, ip) != 0) {
+      pthread_join(worker, NULL);
+    }
   }
-  pthread_join(server, NULL);
+  if (strcmp(comp_ip, ip) == 0) {
+    pthread_join(server, NULL);
+  }
 
   FILE* finalagg = fopen("./finalaggregate", "w");
   char agg_base[20] = "./out";
